6e5f86eaa3144b079b690f7ff5016f64cc1957b3
Joe-Erinjery <joeerinjery7@gmail.com>
2025-06-25 22:36:17 +0530

    Create settingsScreen.js

---
+++ new-folder/src/settingsScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Switch } from 'react-native';
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>Enable Dark Mode</Text>
+      <Switch />
+    </View>
+  );
+};
+
+export default SettingsScreen;
