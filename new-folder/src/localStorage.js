5381f8b7302207bbd19fdbf830c225ef3693dabb
Joe-Erinjery <joeerinjery7@gmail.com>
2025-06-25 22:34:59 +0530

    Create localStorage.js

---
+++ new-folder/src/localStorage.js
@@ -0,0 +1,19 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const saveData = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    console.log('Storage Error:', e);
+  }
+};
+
+export const loadData = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  } catch (e) {
+    console.log('Load Error:', e);
+    return null;
+  }
+};
