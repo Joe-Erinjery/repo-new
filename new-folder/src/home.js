5f7b1a542cc94370cb5c3183215de85a8b2fc27b
Joe-Erinjery <joeerinjery7@gmail.com>
2025-06-25 22:34:05 +0530

    Create home.js

---
+++ new-folder/src/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Welcome to the App!</Text>
+      {/* Logo and items list here */}
+    </View>
+  );
+};
+
+export default Home;
